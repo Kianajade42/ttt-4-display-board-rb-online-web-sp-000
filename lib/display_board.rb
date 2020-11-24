0d496df0d8f48d980abd1a59e78d7af9da64f1ac
Kiana Long <kianajade42.kl@gmail.com>
2020-11-24 15:01:45 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
-board=
-puts"   |   |   "
-puts"-----------"
-puts"   |   |   "
-puts"-----------"
-puts"   |   |   "
+board=[
+"   |   |   "
+"-----------"
+"   |   |   "
+"-----------"
+"   |   |   "]
 def display_board(board)
 end
